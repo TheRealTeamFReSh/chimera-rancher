086220bf928699239c5577c51b57c67300e3c95a
Carlo Supina <cdsupina@gmail.com>
2022-06-21 00:50:11 -0500

    implment idle ai for pig

--- src/animals/behavior.rs
+++ src/animals/behavior.rs
@@ -1,21 +1,103 @@
 use bevy::prelude::*;
 use bevy_rapier2d::prelude::*;
+use rand::Rng;
+use rand_distr::{Distribution, UnitCircle};
+use std::time::Duration;
 
-use super::AnimalComponent;
+use super::{AnimalComponent, AnimalStats};
+
+const IDLE_DURATION_SPREAD: f32 = 2.0;
+const ROUND_ZERO_RANGE: f32 = 10.0;
 
 pub enum AnimalBehavior {
-    Idle { timer: Timer, base_duration: f32 },
+    Idle {
+        timer: Timer,
+        base_duration: f32,
+        direction: Vec2,
+        is_moving: bool,
+    },
 }
 
-pub fn animal_behavior_system(mut animal_query: Query<(&AnimalComponent, &mut Velocity)>) {
-    for (animal, mut vel) in animal_query.iter_mut() {
-        match &animal.behavior {
+pub fn animal_behavior_system(
+    time: Res<Time>,
+    mut animal_query: Query<(&mut AnimalComponent, &mut Velocity, &mut Sprite)>,
+) {
+    for (mut animal, mut vel, mut sprite) in animal_query.iter_mut() {
+        let stats = animal.stats;
+        match &mut animal.behavior {
             AnimalBehavior::Idle {
                 timer,
                 base_duration,
-            } => animal_idle(timer, base_duration),
+                direction,
+                is_moving,
+            } => animal_idle(
+                &mut vel,
+                &mut sprite,
+                &time,
+                timer,
+                base_duration,
+                direction,
+                is_moving,
+                stats,
+            ),
         }
     }
 }
 
-pub fn animal_idle(timer: &Timer, base_duration: &f32) {}
+pub fn animal_idle(
+    vel: &mut Velocity,
+    sprite: &mut Sprite,
+    time: &Time,
+    timer: &mut Timer,
+    base_duration: &f32,
+    direction: &mut Vec2,
+    is_moving: &mut bool,
+    stats: AnimalStats,
+) {
+    timer.tick(time.delta());
+
+    if timer.just_finished() {
+        timer.set_duration(Duration::from_secs_f32(
+            base_duration
+                - rand::thread_rng().gen_range(-IDLE_DURATION_SPREAD..IDLE_DURATION_SPREAD),
+        ));
+        timer.reset();
+
+        let dir: [f32; 2] = UnitCircle.sample(&mut rand::thread_rng());
+        println!("{:?} is from the unit circle.", dir);
+
+        direction.x = dir[0];
+        direction.y = dir[1];
+
+        sprite.flip_x = direction.x < 0.0;
+
+        *is_moving ^= true;
+    }
+
+    if *is_moving {
+        if vel.linvel.x.abs() < (stats.speed * direction.x).abs() {
+            vel.linvel.x += stats.accel * direction.x;
+        }
+
+        if vel.linvel.y.abs() < (stats.speed * direction.y).abs() {
+            vel.linvel.y += stats.accel * direction.y;
+        }
+    } else if (vel.linvel.x.abs().powf(2.0) + vel.linvel.y.abs().powf(2.0)).sqrt()
+        - ROUND_ZERO_RANGE
+        < 0.0
+    {
+        vel.linvel.x = 0.0;
+        vel.linvel.y = 0.0;
+    } else {
+        if vel.linvel.x > 0.0 {
+            vel.linvel.x -= stats.decel * direction.x.abs();
+        } else {
+            vel.linvel.x += stats.decel * direction.x.abs();
+        }
+        if vel.linvel.y > 0.0 {
+            vel.linvel.y -= stats.decel * direction.y.abs();
+        } else {
+            vel.linvel.y += stats.decel * direction.y.abs();
+        }
+    }
+}
--- src/animals/mod.rs
+++ src/animals/mod.rs
@@ -1,5 +1,6 @@
 use bevy::prelude::*;
 use bevy_rapier2d::prelude::*;
+use rand_distr::{Distribution, UnitCircle};
 
 use self::behavior::AnimalBehavior;
 
@@ -9,7 +10,8 @@ pub struct AnimalsPlugin;
 
 impl Plugin for AnimalsPlugin {
     fn build(&self, app: &mut App) {
-        app.add_startup_system(spawn_test_system);
+        app.add_startup_system(spawn_test_system)
+            .add_system(behavior::animal_behavior_system);
     }
 }
 
@@ -46,18 +48,33 @@ pub fn spawn_animal(
 }
 
 fn spawn_pig(position: Vec2, commands: &mut Commands, asset_server: &AssetServer) {
+    let mut random_direction = Vec2::new(0.0, 0.0);
+    let dir: [f32; 2] = UnitCircle.sample(&mut rand::thread_rng());
+    random_direction.x = dir[0];
+    random_direction.y = dir[1];
+
     commands
         .spawn_bundle(SpriteBundle {
             texture: asset_server.load("pig.png"),
             ..default()
         })
         .insert(Transform::from_translation(position.extend(0.0)))
+        .insert(Velocity {
+            linvel: Vec2::new(0.0, 0.0),
+            angvel: 0.0,
+        })
         .insert(AnimalComponent {
             behavior: AnimalBehavior::Idle {
                 timer: Timer::from_seconds(2.0, false),
-                base_duration: 2.0,
+                base_duration: 2.5,
+                direction: random_direction,
+                is_moving: false,
             },
-            speed: 3.0,
+            stats: AnimalStats {
+                speed: 60.0,
+                accel: 1.5,
+                decel: 7.0,
+            },
         })
         .insert(RigidBody::Dynamic)
         .insert(Collider::cuboid(25.0, 10.0));
@@ -66,5 +83,12 @@ fn spawn_pig(position: Vec2, commands: &mut Commands, asset_server: &AssetServer
 #[derive(Component)]
 pub struct AnimalComponent {
     behavior: AnimalBehavior,
+    stats: AnimalStats,
+}
+
+#[derive(Clone, Copy)]
+pub struct AnimalStats {
     speed: f32,
+    accel: f32,
+    decel: f32,
 }
