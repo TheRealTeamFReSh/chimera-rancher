f4e770d4e5ffd68291e066c132904378ecdd3580
Carlo Supina <cdsupina@gmail.com>
2022-06-25 13:48:30 -0500

    click to spawn chimeras

--- src/spells/mod.rs
+++ src/spells/mod.rs
@@ -1,10 +1,14 @@
 use bevy::prelude::*;
 
+use crate::GameStates;
+
 mod spawn_chimera;
 pub struct SpellsPlugin;
 
 impl Plugin for SpellsPlugin {
     fn build(&self, app: &mut App) {
-        todo!()
+        app.add_system_set(
+            SystemSet::on_update(GameStates::Game).with_system(spawn_chimera::spawn_chimera_system),
+        );
     }
 }
